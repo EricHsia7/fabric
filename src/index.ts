2a56108fc0be1bc185b435ec09dd01cb59e6ee57
Eric <54091924+EricHsia7@users.noreply.github.com>
2023-12-22 22:23:16 +0800

    update

--- src/index.ts
+++ src/index.ts
@@ -18,6 +18,30 @@ import './fabric/index.css';
 var ripple = require('@erichsia7/ripple');
 var localforage = require('localforage');
 
+
+//for development
+
+const ErrorStackParser = require('error-stack-parser');
+const StackTrace = require('stacktrace-js');
+
+window.onerror = async function (message, source, lineno, colno, error) {
+  StackTrace.fromError(error).then(function (stackTrace) {
+    var parsedStackTrace = stackTrace.map(function (frame) {
+      return {
+        functionName: frame.functionName,
+        fileName: frame.fileName,
+        lineNumber: frame.lineNumber,
+        columnNumber: frame.columnNumber
+      };
+    });
+    console.log('%c ----------', 'color: #888;');
+    parsedStackTrace.forEach((e) => {
+      console.error(`func: ${e.functionName}\npath: ${e.fileName}\nlocation: L${e.lineNumber} C${e.columnNumber}`);
+    });
+  });
+};
+
+
 window.fabric_initialize = function () {
   canvas.addEventListener(
     'touchstart',
