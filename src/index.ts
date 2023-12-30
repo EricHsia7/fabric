e716fac2fb3d0a2be3cbad203c660ebce7d055e0
Eric <54091924+EricHsia7@users.noreply.github.com>
2023-12-30 09:38:40 +0800

    update

--- src/index.ts
+++ src/index.ts
@@ -123,6 +123,7 @@ window.fabric_initialize = function () {
   resizeFabric();
   loadContent();
   checkPassive();
+  /*
 
   document.querySelector('.tools_container button[group="2"]').addEventListener('click', function () {
     openColorPlate();
@@ -131,6 +132,7 @@ window.fabric_initialize = function () {
   document.querySelector('.fabric_color_plate_close button').addEventListener('click', function () {
     closeColorPlate();
   });
+  */
 };
 
 export default window.fabric_initialize;
