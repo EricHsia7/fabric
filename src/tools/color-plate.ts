f9bfc6c05b264773f6fb5f591f5326cd29549a4c
Eric <54091924+EricHsia7@users.noreply.github.com>
2023-12-31 11:08:34 +0800

    update

--- src/tools/color-plate.ts
+++ src/tools/color-plate.ts
@@ -1,7 +1,7 @@
 import { listFabricColors, colorToHex } from './color.ts';
 import { uuidv4 } from '../index.ts';
 
-function fc_animation(index, time, delay, m, selector, initial, quantity, index_offset) {
+function fc_animation(index, time, delay, m, selector, initial, quantity, df) {
   if (m === 0) {
     var fc_opacity = 0;
     var fc_scale = 0.66;
@@ -12,7 +12,7 @@ function fc_animation(index, time, delay, m, selector, initial, quantity, index_
     var fc_scale = 1;
     var fc_direction = 'reverse';
   }
-  return `.tools_container .${selector} button:nth-child(${quantity - index + index_offset + 1}){transform:scale(${fc_scale}) translateX(calc(var(--index-offset) * 50px));opacity:${fc_opacity};animation-duration: ${time}ms;animation-name: scale;animation-timing-function: ease-out;animation-fill-mode: forwards;animation-delay: ${initial + (index - 1) * delay}ms;animation-direction: ${fc_direction};}`;
+  return `.tools_container .${selector} button:nth-child(${quantity - index + 1}){transform:scale(${fc_scale});opacity:${fc_opacity};animation-duration: ${time}ms;animation-name: scale;animation-timing-function: ease-out;animation-fill-mode: forwards;animation-delay: ${initial + (index - 1) * delay}ms;animation-direction: ${fc_direction};}`;
 }
 
 function fc_s1(m, selector) {
@@ -24,18 +24,18 @@ function fc_s1(m, selector) {
     var fc_opacity = 1;
     var fc_scale = 1;
   }
-  return `.tools_container .${selector} button {transform:scale(${fc_scale}) translateX(calc(var(--index-offset) * 50px));opacity: ${fc_opacity};}`;
+  return `.tools_container .${selector} button {transform:scale(${fc_scale});opacity: ${fc_opacity};}`;
 }
 
-function fc_getHTML(colorObj,index_offset) {
+function fc_getHTML(colorObj) {
   var hex = colorToHex(colorObj);
-  return `<button id="${colorObj.id}" onclick="setPenColor('${colorObj.id}')" style="--index-offset:${index_offset};"><div class="fabric_color"><div class="fabric_color_c"><div class="fabric_color_light" style="--fc-color:${hex.light.hex}"></div><div class="fabric_color_dark" style="--fc-color:${hex.dark.hex}"></div></div></div></button>`;
+  return `<button id="${colorObj.id}" onclick="setPenColor('${colorObj.id}')"><div class="fabric_color"><div class="fabric_color_c"><div class="fabric_color_light" style="--fc-color:${hex.light.hex}"></div><div class="fabric_color_dark" style="--fc-color:${hex.dark.hex}"></div></div></div></button>`;
 }
 
-function generateColorPlateSkeletonScreen(index_offset) {
+function generateColorPlateSkeletonScreen() {
   var html = [];
   for (var i = 0; i < 7; i++) {
-    html.push(fc_getHTML({ id: 'skeleton-screen-' + uuidv4(), light: { type: 'rgb', r: 242, g: 242, b: 242 }, dark: { type: 'rgb', r: 46, g: 46, b: 46 } },index_offset));
+    html.push(fc_getHTML({ id: 'skeleton-screen-' + uuidv4(), light: { type: 'rgb', r: 242, g: 242, b: 242 }, dark: { type: 'rgb', r: 46, g: 46, b: 46 } }));
   }
   document.querySelector('.fabric_color_plate').innerHTML = html.join('');
 }
@@ -44,7 +44,7 @@ function loadColorPlate() {
   listFabricColors().then(function (list) {
     var html = [];
     list.forEach(function (item) {
-      html.push(fc_getHTML(item,0));
+      html.push(fc_getHTML(item));
     });
     document.querySelector('.fabric_color_plate').innerHTML = html.join('');
   });
@@ -54,25 +54,31 @@ export function openColorPlate() {
   const quantity = 6;
   const time = 180;
   const delay = 45;
-  const index_offset = Math.floor(document.querySelector('.fabric_color_plate').scrollLeft / 50) || 0;
-  generateColorPlateSkeletonScreen();
 
-  var fcp = document.querySelector('.fabric_color_plate');
-  fcp.style.display = 'block';
-  var fcpc = document.querySelector('.fabric_color_plate_close');
-  fcpc.style.display = 'grid';
+  generateColorPlateSkeletonScreen();
 
   var css = [];
   for (var i = quantity; i > 0; i--) {
-    css.push(fc_animation(i, time, delay, 1, 'tools_button', 0, quantity, 0));
-  }
-  for (var i = quantity; i > 0; i--) {
-    css.push(fc_animation(i, time, delay, 0, 'fabric_color_plate', (delay * quantity + time) / 2, quantity, index_offset));
-  }
-  for (var i = quantity; i > 0; i--) {
-    css.push(fc_animation(i, time, delay, 0, 'fabric_color_plate_close', 0, quantity, 0));
+    css.push(fc_animation(i, time, delay, 1, 'tools_button', 0, quantity, ''));
   }
   document.querySelector('#fabric-color-plate-animation').innerHTML = css.join('');
+
+  var fcp = document.querySelector('.fabric_color_plate');
+  fcp.style.display = 'block';
+
+  css = [];
+  for (var i = quantity; i > 0; i--) {
+    css.push(fc_animation(i, time, delay, 0, 'fabric_color_plate', (delay * quantity + time) / 2, quantity));
+  }
+  document.querySelector('#fabric-color-plate-animation').innerHTML += css.join('');
+  css = [];
+  for (var i = quantity; i > 0; i--) {
+    css.push(fc_animation(i, time, delay, 0, 'fabric_color_plate_close', 0, quantity, ''));
+  }
+  document.querySelector('#fabric-color-plate-animation').innerHTML += css.join('');
+  var fcpc = document.querySelector('.fabric_color_plate_close');
+  fcpc.style.display = 'grid';
+
   document.querySelectorAll('.fabric_color_plate button')[0].addEventListener(
     'animationend',
     function () {
@@ -88,16 +94,15 @@ export function closeColorPlate() {
   const quantity = 6;
   const time = 180;
   const delay = 45;
-  const index_offset = Math.floor(document.querySelector('.fabric_color_plate').scrollLeft / 50) || 0;
 
   var css = [];
   for (var i = quantity; i > 0; i--) {
-    css.push(fc_animation(i, time, delay, 0, 'tools_button', (delay * quantity + time) / 2, quantity, 0));
+    css.push(fc_animation(i, time, delay, 0, 'tools_button', (delay * quantity + time) / 2, quantity, ''));
   }
   document.querySelector('#fabric-color-plate-animation').innerHTML = css.join('');
   css = [];
   for (var i = quantity; i > 0; i--) {
-    css.push(fc_animation(i, time, delay, 1, 'fabric_color_plate', 0, quantity, index_offset));
+    css.push(fc_animation(i, time, delay, 1, 'fabric_color_plate', 0, quantity, '.fabric_color'));
   }
   document.querySelector('#fabric-color-plate-animation').innerHTML += css.join('');
   document.querySelector('#fabric-color-plate-animation').innerHTML += fc_animation(1, time, delay, 1, 'fabric_color_plate_close', 0, 1, '');
